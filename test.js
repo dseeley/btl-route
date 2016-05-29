1ef40fceb082b9503cb4aba75519443704de7b30
Dougal Seeley <dougal+s4gitlab@s4nity.com>
2016-05-29 17:40:10 +0100

    Improve output

--- test.js
+++ test.js
@@ -5,32 +5,40 @@ var app = angular.module('app', [
 ]);
 
 app.config(['btlRouteProvider', '$locationProvider', function (btlRouteProvider, $locationProvider)
-    {
-        // btlRouteProvider.printDebug(true);
-        // btlRouteProvider.ignoreNested(false);
+{
+    btlRouteProvider.printDebug(true);
+    // btlRouteProvider.ignoreNested(false);
 
-        // $locationProvider.html5Mode(true);
-        $locationProvider.hashPrefix('!');
-    }]);
+    // $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+}]);
+
+app.run(['$rootScope', '$location', function ($rootScope, $location)
+{
+    $rootScope.location = $location;
+}]);
 
 app.controller('appController', ['$scope', '$window', 'btlRoute', 'timerService', function ($scope, $window, btlRoute, timerService)
 {
     timerService.instrument($scope);
 }]);
 
-
-app.service("timerService", ['$interval', function ($interval) {
+app.service("timerService", ['$interval', function ($interval)
+{
     return {
-        instrument: function instrument($scope) {
+        instrument: function instrument($scope)
+        {
             var scopeCreated = Date.now();
-            var computeAge = function () {
+            var computeAge = function ()
+            {
                 var delta = Date.now() - scopeCreated;
                 $scope.ageMs = delta;
                 $scope.age = Math.floor(delta / 1000);
             };
             computeAge();
             var intervalPromise = $interval(computeAge, 1000);
-            $scope.$on("$destroy", function () {
+            $scope.$on("$destroy", function ()
+            {
                 $interval.cancel(intervalPromise);
             })
         }
